a1d56494fa3090daf9d70da57536a43d272885c8
Avichal2310 <119150433+Avichal2310@users.noreply.github.com>
2022-11-27 15:27:46 +0530

    Add files via upload

---
+++ if1.c
@@ -0,0 +1,14 @@
+/*
+Example 1 of if
+Write a program to input a character. If M is pressed print Male else print Female
+*/
+main(){
+    char ch;    
+	printf("Enter a character");
+	scanf("%c",&ch);
+	if(ch=='M')
+		printf("Male");
+	else
+		printf("Female");
+}
+
