c6be47b97479ddb70e617124f087c327bd7da357
Avichal2310 <119150433+Avichal2310@users.noreply.github.com>
2022-11-27 15:03:01 +0530

    Add files via upload

---
+++ EvenOdd.c
@@ -0,0 +1,13 @@
+/*
+WAP to input a number and check it to be even or odd.
+*/
+main(){
+	int n;
+	printf("Enter a no");
+	scanf("%d",&n);
+	if(n%2==0)
+		printf("%d is even",n);
+	else
+		printf("%d is odd",n);
+}
+
